ccd2a97677b9d7691036aa1332d59aec02c6afce
mrharshamin93-code <mrharshamin93@gmail.com>
2026-07-19 14:02:27 -0400

    Configure path alias in Vite config
    Added path alias for easier imports.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  server: {
-    port: 5173,
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
